a3e4b3b0bc46ec05b946b812f5aa88497d1c84f1
Sahil Arora <31217411+GomberSahil@users.noreply.github.com>
2018-10-10 21:01:05 +0530

    ss

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('common/landing');
+  res.render('common/landing', { title: 'HomePage' });
 });
 
 router.get('/contact', (req, res) => {
-  res.render('common/contact');
+  res.render('common/contact',{ title: 'contact'});
 });
 
 router.get('/about', (req, res) => {
