c2a46670b3be750debde6ace14851eeabbbeef51
Sahil Arora <31217411+GomberSahil@users.noreply.github.com>
2018-10-10 20:03:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const routes = require('./routes');
-const serviceRoutes = require('./routes/service').default;
+const serviceRoutes = require('./routes/service');
 
 const app = express();
 
