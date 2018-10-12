29cbb8a5b6b56be7c095b14a76c808b9e80d68a0
Sahil Arora <31217411+GomberSahil@users.noreply.github.com>
2018-10-12 18:13:07 +0530

    some common changes

--- routes/service.js
+++ routes/service.js
@@ -2,27 +2,31 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/services/packers-and-movers', (req, res) => {
+router.get('/', (req, res) => {
+  res.render('services/services');
+});
+
+router.get('/packers-and-movers', (req, res) => {
   res.render('service/packers-and-movers');
 });
 
-router.get('/services/home-shifting', (req, res) => {
+router.get('/home-shifting', (req, res) => {
   res.render('service/home-shifting');
 });
 
-router.get('/services/corporate-relocation', (req, res) => {
+router.get('/corporate-relocation', (req, res) => {
   res.render('service/corporate-relocation');
 });
 
-router.get('/services/industrial-relocation', (req, res) => {
+router.get('/industrial-relocation', (req, res) => {
   res.render('service/industrial-relocation');
 });
 
-router.get('/services/logistics', (req, res) => {
+router.get('/logistics', (req, res) => {
   res.render('service/logistics');
 });
 
-router.get('/services/single-car-carrier', (req, res) => {
+router.get('/single-car-carrier', (req, res) => {
   res.render('service/single-car-carrier');
 });
 
