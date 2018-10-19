9175935297df2adc8e2557726faad085bbd136f1
Dheeraj Arora <dheeraj.arora1997@yahoo.com>
2018-10-20 01:37:55 +0530

    UI changes

--- public/scripts/animation.js
+++ public/scripts/animation.js
@@ -10,4 +10,15 @@ $(document).ready(function () {
   $(".landing-blog-item,.service-list-item").hover(function (e) {
     $(this).toggleClass("active");
   });
+  $(".scroll-arrow").on("click", function () {
+    $(window).scrollTop(0);
+  });
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 150) {
+      $('.scroll-arrow').fadeIn();
+    } else {
+      $('.scroll-arrow').fadeOut();
+    }
+  });
 });
