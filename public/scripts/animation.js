e5268fbfc6aeef0cfcc95f6a3cd9640e9491dcba
Dheeraj Arora <dheeraj.arora1997@yahoo.com>
2018-10-31 01:59:55 -0700

    ui changes

--- public/scripts/animation.js
+++ public/scripts/animation.js
@@ -1,31 +1,39 @@
-$(document).ready(function () {
+$(document).ready(function() {
   function addActive() {
     $(this).addClass("active");
-    console.log('#someButton was clicked');
   }
 
+  function removeActive() {
+    $(this).removeClass("active");
+  }
 
+  $(".landing-blog-item,.service-list-item").hover(addActive, removeActive);
+  $(".scroll-arrow").on("click", returnToTopOfScreen);
 
+  function returnToTopOfScreen() {
+    $("html, body").animate(
+      {
+        scrollTop: 0
+      },
+      500
+    );
+  }
 
-  $(".landing-blog-item,.service-list-item").hover(function (e) {
-    $(this).toggleClass("active");
-  });
-  $(".scroll-arrow").on("click", function () {
-    $(window).scrollTop(0);
-  });
-
-  $(window).scroll(function () {
+  $(window).scroll(function() {
     if ($(this).scrollTop() > 150) {
-      $('.scroll-arrow').fadeIn();
+      $(".scroll-arrow").fadeIn();
     } else {
-      $('.scroll-arrow').fadeOut();
+      $(".scroll-arrow").fadeOut();
     }
   });
-  $(window).bind("resize", function () {
-    if ($(this).width() < 991) {
-      $('.timeline > li.inverted').removeClass('timeline-inverted')
-    } else {
-      $('.timeline > li.inverted').addClass('timeline-inverted')
-    }
-  }).trigger('resize');
+
+  $(window)
+    .bind("resize", function() {
+      if ($(this).width() < 991) {
+        $(".timeline > li.inverted").removeClass("timeline-inverted");
+      } else {
+        $(".timeline > li.inverted").addClass("timeline-inverted");
+      }
+    })
+    .trigger("resize");
 });
