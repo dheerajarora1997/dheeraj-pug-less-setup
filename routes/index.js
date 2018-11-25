173865c7a27df1f63b134ec900076d2c787bd728
Dheeraj Arora <dheeraj.arora1997@yahoo.com>
2018-11-25 09:53:38 +0530

    UI changes

--- routes/index.js
+++ routes/index.js
@@ -3,38 +3,26 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('common/landing', {
-    title: 'HomePage',
-  });
+  res.render('common/landing');
 });
 
 router.get('/contact', (req, res) => {
-  res.render('common/contact', {
-    title: 'contact',
-  });
+  res.render('common/contact');
 });
 
 router.get('/about', (req, res) => {
-  res.render('common/about', {
-    title: 'about',
-  });
+  res.render('common/about');
 });
 
 router.get('/portfolio', (req, res) => {
-  res.render('common/portfolio', {
-    title: 'portfolio',
-  });
+  res.render('common/portfolio');
 });
 
 router.get('/testimonial', (req, res) => {
-  res.render('common/testimonial', {
-    title: 'Testimonial',
-  });
+  res.render('common/testimonial');
 });
 
 router.get('/services', (req, res) => {
-  res.render('common/services', {
-    title: 'services',
-  });
+  res.render('common/services');
 });
 module.exports = router;
